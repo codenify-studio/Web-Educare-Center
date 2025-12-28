3db33b4e55c77583bebae8c32993ae4eb1b05218
codenify-studio <mdfarhankhan320@gmail.com>
2025-12-28 23:40:39 +0530

    marque slider done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,3 +5,41 @@ toggleBtn.addEventListener("click", () => {
   contentRow.classList.toggle("expanded");
   toggleBtn.classList.toggle("rotated");
 });
+
+$(".myslider").owlCarousel({
+  loop: true,
+  autoplay: true,
+  slideTransition: "linear",
+  autoplayHoverPause: false,
+  autoplaySpeed: 8000,
+  autoplayTimeout: 0,
+  smartSpeed: 8000,
+  margin: 40,
+  nav: false,
+  dots: false,
+  mouseDrag: false, 
+  touchDrag: false, 
+  pullDrag: false,  
+  freeDrag: false,
+  // lazyLoad: true,
+  responsive: {
+    0: {
+      items: 1,
+      loop: true,
+    },
+
+    600: {
+      items: 2,
+      loop: true,
+    },
+    768: {
+      items: 3,
+      loop: true,
+    },
+    992: {
+      items: 3,
+      loop: true,
+    },
+    1200: { items: 2 },
+  },
+});
